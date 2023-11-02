3aac818d006431e7937b525f29f0eac790c25320
shibe7202 <46942349+shibe7202@users.noreply.github.com>
2023-11-02 14:31:21 -0700

    Implement function to create new grid.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,36 @@
 let container = document.querySelector('#container');
 
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement('div');
-    div.classList.add('squares');
-    container.appendChild(div);
-}
+function create(n = 256) {
+    for (let i = 0; i < n; i++) {
+        let div = document.createElement('div');
+        div.classList.add('squares');
+        container.appendChild(div);
+    }
 
-const grid = document.querySelectorAll('.squares');
+    const grid = document.querySelectorAll('.squares');
 
-grid.forEach((square) => {
-    square.addEventListener('mouseover', (event) => {
-        event.currentTarget.style.backgroundColor = 'purple';
+    grid.forEach((square) => {
+        square.addEventListener('mouseover', (event) => {
+            event.currentTarget.style.backgroundColor = 'purple';
+        })
     })
+}
+create();
+
+const btn = document.querySelector('#button');
+btn.addEventListener('click', () => {
+    let size = prompt('Enter squares per side');
+    if (size <= 100 && size > 0) {
+        container.innerHTML = '';
+        create(size * size);
+
+        const grid = document.querySelectorAll('.squares');
+        const squareDimensions = 800 / size;
+        
+        grid.forEach((square) => {
+            square.style.height = "" + squareDimensions + "px";
+            square.style.width = "" + squareDimensions + "px";
+        })
+    }
 })
+
